4ea3f0e22c0467ff6f66b94faeae28c2ad04fb29
Felipe Souza <felipesouza036@gmail.com>
2023-09-19 14:23:49 -0300

    feat: Apply general font Roboto Flex

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -5,6 +5,7 @@ const GlobalStyles = createGlobalStyle`
         padding: 0;
         margin: 0;
         box-sizing: border-box;
+        font-family: "Roboto Flex", sans-serif;
     }
 
     html {
