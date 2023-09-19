46a5c7214e038822791b7bbeaae7e522774293ef
Felipe Souza <felipesouza036@gmail.com>
2023-09-19 11:53:39 -0300

    feat: Add Theme Provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { ThemeProvider } from "styled-components";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import theme from "./styles/theme.ts";
+import GlobalStyles from "./styles/GlobalStyles.ts";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
---
+++ src/styles/theme.ts
@@ -0,0 +1,29 @@
+const theme = {
+  colors: {
+    primary: "#ED6D25",
+    primaryHover: "#F08242",
+    primaryDisabled: "#E25F12",
+    secondary: "",
+    background: "#D9E6F7",
+    placeholder: "#868686",
+    lightGray: "#EFF3F8",
+  },
+  sizes: {
+    tiny: "0.4rem",
+    xsmall: "0.8rem",
+    small: "1.2rem",
+    medium: "1.6rem",
+    big: "2.4rem",
+    xbig: "3.2rem",
+    giant: "4rem",
+    xgiant: "4.8rem",
+    huge: "6.4rem",
+    xhuge: "9.6rem",
+  },
+  medias: {
+    tablet: "@media (max-width: 768px)",
+    mobile: "@media (max-width: 420px)",
+  },
+};
+
+export default theme;
