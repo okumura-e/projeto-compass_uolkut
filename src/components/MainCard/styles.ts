bbe0be5c66738de7b7cab408c0a20575cd05177d
Felipe Souza <felipesouza036@gmail.com>
2023-09-20 10:50:03 -0300

    feat (Main Card): Add styles

---
+++ src/components/MainCard/styles.ts
@@ -0,0 +1,201 @@
+import styled, { css } from "styled-components";
+
+export const Container = styled.article`
+  ${({ theme }) => {
+    return css`
+      display: flex;
+      flex-direction: column;
+      max-width: 320px;
+      padding: 2.4rem;
+
+      min-height: 940px;
+
+      color: ${theme.colors.content};
+
+      background: ${theme.colors.secondaryBg};
+      border-radius: 16px;
+    `;
+  }}
+`;
+
+export const Title = styled.h2`
+  ${({ theme }) => {
+    return css`
+      font-size: 2rem;
+      font-weight: 400;
+
+      margin-bottom: 2rem;
+    `;
+  }}
+`;
+
+export const Thought = styled.p`
+  ${({ theme }) => {
+    return css`
+      position: relative;
+
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      padding-left: 1.2rem;
+      margin-bottom: 4rem;
+
+      background: ${theme.colors.lightGray};
+      border-radius: 10px;
+
+      width: 100%;
+      min-height: 32px;
+
+      font-size: 1.2rem;
+
+      &::before {
+        position: absolute;
+        left: 10px;
+        top: -4px;
+        content: "";
+        display: block;
+
+        width: 0;
+        height: 0;
+        border-left: 5px solid transparent;
+        border-right: 5px solid transparent;
+
+        border-bottom: 5px solid ${theme.colors.lightGray};
+      }
+    `;
+  }}
+`;
+
+export const StatisticsContainer = styled.section`
+  ${({ theme }) => {
+    return css`
+      display: grid;
+      grid-template-columns: repeat(auto-fill, 100px);
+      gap: 3.2rem;
+      width: 100%;
+
+      margin-bottom: 4.8rem;
+    `;
+  }}
+`;
+
+export const Statistic = styled.p`
+  ${({ theme }) => {
+    return css`
+      width: fit-content;
+      display: flex;
+      flex-direction: column;
+      gap: 0.8rem;
+    `;
+  }}
+`;
+
+export const StatisticTitle = styled.span`
+  ${({ theme }) => {
+    return css`
+      font-size: 1.6rem;
+    `;
+  }}
+`;
+
+export const StatisticData = styled.span`
+  ${({ theme }) => {
+    return css`
+      display: flex;
+
+      align-items: center;
+
+      gap: 0.6rem;
+      font-size: 1.6rem;
+
+      svg {
+        width: 2.4rem;
+        height: auto;
+      }
+    `;
+  }}
+`;
+
+export const DetailsDataContainer = styled.section`
+  ${({ theme }) => {
+    return css`
+      display: flex;
+      flex-direction: column;
+      gap: 2.4rem;
+
+      color: ${theme.colors.lightestGray};
+      font-size: 1.6rem;
+
+      strong {
+        color: ${theme.colors.content};
+        font-weight: 400;
+      }
+    `;
+  }}
+`;
+
+export const Detail = styled.p`
+  ${({ theme }) => {
+    return css``;
+  }}
+`;
+
+export const PreferencesContainer = styled.section`
+  ${({ theme }) => {
+    return css`
+      display: flex;
+      flex-direction: column;
+      margin-top: auto;
+      gap: 3.2rem;
+    `;
+  }}
+`;
+
+export const Preference = styled.p`
+  ${({ theme }) => {
+    return css`
+      font-size: 1.6rem;
+      color: ${theme.colors.lightestGray};
+    `;
+  }}
+`;
+
+export const Tag = styled.span`
+  ${({ theme }) => {
+    return css`
+      padding: 0.2rem 1.6rem;
+      font-size: 1.4rem;
+      color: ${theme.colors.content};
+
+      border: 1px solid ${theme.colors.primary};
+      border-radius: 9999px;
+
+      &:first-child {
+        margin-left: 1.2rem;
+      }
+      &:not(:last-child) {
+        margin-right: 1.4rem;
+      }
+    `;
+  }}
+`;
+
+export const SeeMoreLink = styled.a`
+  ${({ theme }) => {
+    return css`
+      text-decoration: none;
+      color: ${theme.colors.primary};
+      transition: color 0.2s ease-in-out;
+
+      margin-left: 1.6rem;
+      font-size: 1.4rem;
+
+      cursor: pointer;
+
+      &:hover {
+        color: ${theme.colors.primaryHover};
+      }
+    `;
+  }}
+`;
