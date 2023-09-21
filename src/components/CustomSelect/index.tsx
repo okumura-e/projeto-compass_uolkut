1813bcbd6f3006b4ea1bd51c69d532bb2f47005f
Felipe Souza <felipesouza036@gmail.com>
2023-09-21 13:29:09 -0300

    feat: Add custom select component

---
+++ src/components/CustomSelect/index.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import { Container, Select, OptionsList, Option } from "./styles";
+
+import { IoIosArrowDown } from "react-icons/io";
+
+interface CustomSelectProps {
+  selected?: string;
+  options: string[];
+  placeholder: string;
+  onSelect: (option: string) => void;
+}
+
+const CustomSelect = ({
+  selected,
+  options,
+  placeholder,
+  onSelect,
+}: CustomSelectProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isHovering, setIsHovering] = useState(false);
+
+  const handleSelectOption = (text: string) => {
+    setIsOpen(false);
+    onSelect(text);
+  };
+
+  return (
+    <Container>
+      <Select
+        isOpened={isOpen}
+        isHovering={isHovering}
+        onMouseEnter={() => setIsHovering(true)}
+        onMouseLeave={() => setIsHovering(false)}
+        onClick={() => setIsOpen((prev) => !prev)}
+      >
+        <span>{selected || placeholder}</span>
+        <IoIosArrowDown />
+      </Select>
+      {isOpen && (
+        <OptionsList>
+          {options.map((text) => (
+            <Option
+              selected={selected === text}
+              onClick={() => handleSelectOption(text)}
+              key={text}
+            >
+              {text}
+            </Option>
+          ))}
+        </OptionsList>
+      )}
+    </Container>
+  );
+};
+
+export default CustomSelect;
