f8adb4b1eebf5afd463fde52435adbf073cb4e50
Felipe Souza <felipesouza036@gmail.com>
2023-09-21 07:51:05 -0300

    feat: Add body background color

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -11,6 +11,10 @@ const GlobalStyles = createGlobalStyle`
     html {
         font-size: 62.5% /* Change the default font-size value from 16px to 10px */;
     }
+
+    body {
+        background: #D9E6F7
+    }
 `;
 
 export default GlobalStyles;
