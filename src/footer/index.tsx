0b73fce3db4890df05aaa71f4a79331f1ff86cf3
JoaoAugusto1374 <tauajoaoaugustoce@gmail.com>
2023-09-21 09:04:50 -0300

    firstcommit

---
+++ src/footer/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>Todos os direitos reservados</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/header/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <div className="logo">
+        <img src="caminho-para-o-seu-logo.png" alt="UolKut" />
+        <h1>UolKut</h1>
+      </div>
+      <div className="seguranca">
+        <p>Centro de Segurança</p>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
