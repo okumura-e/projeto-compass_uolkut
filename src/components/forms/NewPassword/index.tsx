7d7b4f5a5e03375ec84d623af6e2b96056ab3ee9
Adriel Itamar <adrielitamar@gmail.com>
2023-09-22 12:31:57 -0300

    feat: add new password form validation

--- src/components/forms/NewPassword/index.tsx
+++ src/components/forms/NewPassword/index.tsx
@@ -13,11 +13,28 @@ import { useNavigate } from "react-router-dom";
 
 import FormButton from "../../FormButton";
 import FormInput from "../../FormInput";
+import { useForm } from "react-hook-form";
 
 const NewPasswordForm = () => {
   const navigate = useNavigate();
 
-  const handleSubmit = () => {
+  const {
+    clearErrors,
+    formState: { errors },
+    handleSubmit,
+    register,
+    watch
+  } = useForm({
+    defaultValues: {
+      code: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  const checkPassword  = watch("password")
+
+  const onSubmit = (data: unknown) => {
     navigate("/profile");
   };
   return (
@@ -25,15 +42,49 @@ const NewPasswordForm = () => {
       <Logo role="img" />
       <Title>Nova senha</Title>
 
-      <FormContainer>
+      <FormContainer
+        onSubmit={handleSubmit(onSubmit, () => setTimeout(clearErrors, 2500))}
+      >
         <InputsContainer>
-          <FormInput type="text" placeholder="Informe o código" />
-          <FormInput type="password" placeholder="Nova senha" />
-          <FormInput type="password" placeholder="Confirmar a senha" />
+          <FormInput type="text" placeholder="Informe o código" 
+            registerField={{
+              ...register("code", {
+                required: { value: true, message: "O código é obrigatório!" },
+                minLength: {
+                  value: 5,
+                  message: "O código deve possuir 5 dígitos!",
+                },
+              }),
+            }}
+            error={errors?.code?.message} 
+          />
+          <FormInput type="password" placeholder="Nova senha" 
+            registerField={{
+              ...register("password", {
+                required: { value: true, message: "A senha é obrigatória!" },
+                minLength: {
+                  value: 6,
+                  message: "A senha deve conter pelo menos 6 dígitos!",
+                },
+              }),
+            }}
+            error={errors?.password?.message} 
+          />
+          <FormInput type="password" placeholder="Confirmar a senha" registerField={{
+            ...register("confirmPassword", {
+              required: { value: true, message: "A senha é obrigatória!" },
+              minLength: {
+                value: 6,
+                message: "A senha deve conter pelo menos 6 dígitos!",
+              },
+              validate: value => value === checkPassword || "As senhas devem ser iguais!"
+            }),
+          }}
+            error={errors?.confirmPassword?.message} />
         </InputsContainer>
 
         <ButtonContainer>
-          <FormButton onClick={handleSubmit} title="Salvar" />
+          <FormButton onClick={handleSubmit(onSubmit, () => setTimeout(clearErrors, 2500))} title="Salvar" />
         </ButtonContainer>
 
         <RememberContainer>
