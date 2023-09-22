135c09ddbd3407a14519fd23648a8feb4bad0266
Adriel Itamar <adrielitamar@gmail.com>
2023-09-21 21:30:08 -0300

    feat (FormInput): create form inputs component

---
+++ src/components/FormInput/index.tsx
@@ -0,0 +1,20 @@
+import {
+    Input,
+  } from "./style";
+  
+  interface InputProps {
+    placeHolder: string;
+    inputType: 'text' | 'password' | 'email' | 'date' ;
+    fullLength: boolean
+  }
+  
+  function FormInput({ placeHolder, inputType, fullLength }: InputProps) {
+    return (
+        <>
+            <Input placeholder={placeHolder} type={inputType} full={fullLength}></Input>
+        </>
+    );
+  }
+  
+  export default FormInput;
+  
