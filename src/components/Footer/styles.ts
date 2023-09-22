273fa6d05c96b670f68fea49e19ff30b58972c8a
Felipe Souza <felipesouza036@gmail.com>
2023-09-22 10:48:09 -0300

    fix (Footer): Invert display none logic

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -2,10 +2,31 @@ import { BsDisplay } from "react-icons/bs";
 import styled, { css } from "styled-components";
 
 export const Container = styled.footer`
-    ${({ theme }) => {
-    return css`
+  ${({ theme }) => {
+    return css`s
+
+      display: none;
+      margin-top: auto;
       background: ${theme.colors.secondaryBg};
       padding: 2.4rem 0;
+
+      ${theme.medias.tablet} {
+        display: block;
+      }
+    `;
+  }}
+`;
+
+export const Text = styled.p`
+  ${({ theme }) => {
+    return css`
+      color: #ed6d25;
+      font-family: Roboto Flex;
+      font-size: 1.125rem;
+      font-style: normal;
+      font-weight: 400;
+      line-height: 1.5rem;
+      text-align: center;
 
       ${theme.medias.mobile} {
         display: none;
@@ -13,23 +34,3 @@ export const Container = styled.footer`
     `;
   }}
 `;
-
-export const Text = styled.p`
-    ${({ theme }) => {
-    return css`
-    color: #ED6D25;
-    font-family: Roboto Flex;
-    font-size: 1.125rem;
-    font-style: normal;
-    font-weight: 400;
-    line-height: 1.5rem;   
-    text-align: center;
-
-    ${theme.medias.mobile} {
-        display: none;
-    }
-`;
-    }}
-
-`;
-
