fe19565a7fdf9a7c69793761ca4c4bc608ceff4d
Felipe Souza <felipesouza036@gmail.com>
2023-09-24 14:10:31 -0300

    fix (FormPage): Remove useless classname

--- src/templates/FormPage/index.tsx
+++ src/templates/FormPage/index.tsx
@@ -1,27 +1,25 @@
-import React from 'react';
-import {Container, LeftSection, Text} from './styles';
-import FormPageHeader from '../../components/FormPageHeader';
-import { Outlet } from 'react-router-dom';
-import Footer from '../../components/Footer';
-
-
-
+import React from "react";
+import { Container, LeftSection, Text } from "./styles";
+import FormPageHeader from "../../components/FormPageHeader";
+import { Outlet } from "react-router-dom";
+import Footer from "../../components/Footer";
 
 function FormPage() {
   return (
-   <> 
-   <FormPageHeader />
-   <Container>
-      <LeftSection className="imagem">
-        <Text>Conecta-se aos seus amigos e familiares usando recados e mensagens instantâneas</Text>
-      </LeftSection>
-      <Outlet />
-    </Container>
+    <>
+      <FormPageHeader />
+      <Container>
+        <LeftSection>
+          <Text>
+            Conecta-se aos seus amigos e familiares usando recados e mensagens
+            instantâneas
+          </Text>
+        </LeftSection>
+        <Outlet />
+      </Container>
 
-  <Footer/>
-    
-  
-</>
+      <Footer />
+    </>
   );
 }
 
