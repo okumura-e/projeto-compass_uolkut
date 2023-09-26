7bd622f70de5a1e5fcdae8d23cfcc0e0c12307e9
Felipe Souza <felipesouza036@gmail.com>
2023-09-26 09:23:39 -0300

    feat (PerfilHeader): Add dynamic username

--- src/templates/EditPerfilPage/index.tsx
+++ src/templates/EditPerfilPage/index.tsx
@@ -2,11 +2,13 @@ import { Container } from "./styles";
 import PerfilHeader from "../../components/PerfilHeader";
 import EditUserCard from "../../components/cards/EditUserCard";
 import EditInfoCard from "../../components/cards/EditInfoCard";
+import { useKeepUser } from "../../hooks/useKeepUser";
 
 const EditPerfilPage = () => {
+  const { user } = useKeepUser();
   return (
     <>
-      <PerfilHeader username="Gabriel Barbosa" />
+      <PerfilHeader username={user?.fullname || ""} />
       <Container>
         <EditUserCard />
         <EditInfoCard />
